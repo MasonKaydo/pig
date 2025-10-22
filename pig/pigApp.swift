c0a2dc2475f53a09d6b02ab8b720d6ce1dc8835c
mason <mkaydo@bsd220.org>
2025-10-22 10:20:15 -0500

    Initial Commit

---
+++ pig/pigApp.swift
@@ -0,0 +1,17 @@
+//
+//  pigApp.swift
+//  pig
+//
+//  Created by mason kaydo on 10/22/25.
+//
+
+import SwiftUI
+
+@main
+struct pigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
